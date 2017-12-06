020bf3e4088bc2206eb40b1cc7f60214c7f2e65d
Antonio Guzman <alguzman1@yahoo.com>
2017-12-05 21:08:01 -0500

    InstaSolution

--- client/src/App.js
+++ client/src/App.js
@@ -1,57 +1,8 @@
-import React, {Component} from 'react'
-import $ from 'jquery'
-import IgPosts from './IgPosts'
+import React from 'react'
 
-class App extends Component {
-  state = {
-    caption: undefined
-  }
-
-  componentDidMount() {
-    this.loadPostFromServer()
-  }
-
-  loadPostFromServer = () => {
-    $.ajax({
-      url:'/api/instaGram',
-      method: 'GET'
-    }).done((response) => {
-      console.log(response)
-      this.setState({post: response.post})
-    })
-  }
-
-  submitPostToServer = () => {
-    const newPost = {
-      title: this.state.title,
-      img: this.state.img,
-      caption: this.state.caption
-    }
-    $.ajex({
-      url: 'api/instaGram',
-      method: 'POST',
-      data: newPost
-    }).done((response) => {
-      console.log(response)
-        this.loadPostFromServer()
-    })
-  }
-}
-
-updateCaption = (e) => this.setState({title: e.target.value})
-
-render(){
-  return(
-    <div>
-      <Header />
-      <form>
-        <label>Insert Caption</label>
-        <input type='text' onChange={this.updateCaption} />
-
-        <button type='button' onClick={this.submitPostToServer}>Submit</button>
-      </form>
-    </div>
-  )
-}
+const App = () => 
+  <div>
+    <h1>Hello World</div>
+  </div>
 
 export default App
